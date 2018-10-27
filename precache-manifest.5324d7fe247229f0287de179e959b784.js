0601d8bcf28c8d4c36cf8db9985d0a63b7449b43
cefimenda <jeffymenda@gmail.com>
2018-10-27 10:15:21 -0400

    Updates

---
+++ precache-manifest.5324d7fe247229f0287de179e959b784.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "94e62bfe4d7874c5a7be78aacd84d9af",
+    "url": "/clicky-game/static/media/Jon.94e62bfe.png"
+  },
+  {
+    "revision": "6a8845f3da07be860f07",
+    "url": "/clicky-game/static/css/main.a26d0fee.chunk.css"
+  },
+  {
+    "revision": "b79ef97cb41f7353c6cc",
+    "url": "/clicky-game/static/js/1.b79ef97c.chunk.js"
+  },
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "3f058f6eca021730d2b2e2e3861f272d",
+    "url": "/clicky-game/static/media/Arya.3f058f6e.jpg"
+  },
+  {
+    "revision": "3c5f79f7556f2b712f2b8fa252f7f9a9",
+    "url": "/clicky-game/static/media/Bran.3c5f79f7.png"
+  },
+  {
+    "revision": "7ebe711518c96f5f8e17355c41e5db7c",
+    "url": "/clicky-game/static/media/Cersei.7ebe7115.png"
+  },
+  {
+    "revision": "f6a53eb5650141798fb5817f7d7fae4c",
+    "url": "/clicky-game/static/media/daenerys.f6a53eb5.jpg"
+  },
+  {
+    "revision": "72638dffd32f615776b20a7d52bbe492",
+    "url": "/clicky-game/static/media/Hound.72638dff.png"
+  },
+  {
+    "revision": "6a8845f3da07be860f07",
+    "url": "/clicky-game/static/js/main.6a8845f3.chunk.js"
+  },
+  {
+    "revision": "a8133dfde901bb6481c8b15b6e1ecb8a",
+    "url": "/clicky-game/static/media/Samwell.a8133dfd.jpg"
+  },
+  {
+    "revision": "18c00ed54a8844b7ab28f3b2b6438b2d",
+    "url": "/clicky-game/static/media/Sansa.18c00ed5.jpg"
+  },
+  {
+    "revision": "73f910e962604a7084628ac413721e4a",
+    "url": "/clicky-game/static/media/Theon.73f910e9.jpg"
+  },
+  {
+    "revision": "8ff41675064a916860db4c8181aac343",
+    "url": "/clicky-game/static/media/Tormund.8ff41675.png"
+  },
+  {
+    "revision": "a883ae70296c2c51dfe272166c2607da",
+    "url": "/clicky-game/static/media/Tyrion.a883ae70.jpg"
+  },
+  {
+    "revision": "467ed8bbf48d86f646163cb6b01a711b",
+    "url": "/clicky-game/static/media/Varys.467ed8bb.png"
+  },
+  {
+    "revision": "873bcfd93f96cd89591b6cbea3ca3515",
+    "url": "/clicky-game/static/media/walkers.873bcfd9.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky-game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0fd5a5f21b8a6a69ceec74f75d8ba52f",
+    "url": "/clicky-game/index.html"
+  }
+];
